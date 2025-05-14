4758ad64dc3009b002bd1ab4ee0c69f496e69f95
Antonio Cruz <antoniocruzglez24@gmail.com>
2025-05-14 10:12:08 -0400

    feat: Creating removeAvatarList helper

---
+++ src/helpers/removeAvatrList.ts
@@ -0,0 +1,19 @@
+import type { Team, Member } from '../data/teamsGroups';
+
+export const removeAvatarList = (
+    id: number, 
+    user: Member, 
+    list: Team[]
+): Team[] => {
+    return list.map(team => {
+        if (team.id === id) {
+            return {
+                ...team,
+                members: team.members.filter(
+                    avatar => avatar.id !== user.id
+                )
+            };
+        }
+        return team;
+    });
+};
